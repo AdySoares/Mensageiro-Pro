747b9b65a0d49d661c5e0164e52b181ac388be2d
adson <adsontosoares@gmail.com>
2023-09-20 16:04:16 -0300

    first commit

---
+++ backend/src/database/seeds/20200904070004-create-default-users.ts
@@ -0,0 +1,25 @@
+import { QueryInterface } from "sequelize";
+
+module.exports = {
+  up: (queryInterface: QueryInterface) => {
+    return queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          name: "Press-Ticket",
+          email: "admin@pressticket.com.br",
+          passwordHash: "$2a$12$0DfqJ7RHK6/DwHFAiDf0PONa7YinuWS9XL9C8oFno0WZTPLMrlUn.",
+          profile: "admin",
+          tokenVersion: 0,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      {}
+    );
+  },
+
+  down: (queryInterface: QueryInterface) => {
+    return queryInterface.bulkDelete("Users", {});
+  }
+};
